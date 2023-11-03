d9b23f7a28e086e5df8dd4c203782334b364e8c9
start.vaadin.com <no-reply@vaadin.com>
2023-11-03 06:02:16 +0000

    Generated project

---
+++ src/main/java/com/hn/electronicossheyla/data/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package com.hn.electronicossheyla.data;
+
+import org.springframework.lang.NonNullApi;
---
+++ src/main/java/com/hn/electronicossheyla/services/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package com.hn.electronicossheyla.services;
+
+import org.springframework.lang.NonNullApi;
